b0ea6ef56ef254a3fe9ed6cc6568c946e08bf6b0
Gabriel Brandino <gabriel.brandino01@gmail.com>
2025-05-31 21:59:42 -0300

    feat: adicionar dados já relacionados nas tabelas para fins de teste (#18)
    * fix: reverter commit (#17)
    
    * feat: adicionar tabela de imagens

---
+++ app/Database/Seeds/02_INSERT_INTO_CIDADE.sql
@@ -0,0 +1,5 @@
+INSERT INTO CIDADE (ID_ESTADO, NOME) VALUES 
+(25, 'Araras'),
+(25, 'Leme'),
+(25, 'Rio Claro'),
+(25, 'Limeira');
---
+++ app/Database/Seeds/04_INSERT_INTO_ENDERECO.sql
@@ -0,0 +1,8 @@
+INSERT INTO ENDERECO (ID_BAIRRO, RUA, NUMERO) VALUES
+(1, 'Rua das Flores', '42'),
+(2, 'Av. Luccas Arruda', '456'),
+(3, 'Rua Carlos', '999'),
+(3, 'Rua do Limoeiro', '01'),
+(4, 'Av. Brasil', '123'),
+(5, 'Rua 7 de Setembro', '58'),
+(6, 'Rua das Laranjeiras', '9');
